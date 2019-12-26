221e7bf16f4f1e047c61765913639d60cd0b4d9c
Krzysztof Szot <krzysztof.szot@cognifide.com>
2019-12-26 18:11:31 +0100

    Updates

---
+++ precache-manifest.80f33b1de5f717f608f25bea3e5bc43e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a01f6e0230d9e6f6813526fd445780a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39cefc4007f6d25f5ae0",
+    "url": "/static/css/main.511b0a34.chunk.css"
+  },
+  {
+    "revision": "71268fef7d97b331a08f",
+    "url": "/static/js/2.0435fbd1.chunk.js"
+  },
+  {
+    "revision": "39cefc4007f6d25f5ae0",
+    "url": "/static/js/main.4094daef.chunk.js"
+  },
+  {
+    "revision": "bf1f4ff12feed79b82c2",
+    "url": "/static/js/runtime-main.09c786ee.js"
+  }
+]);
