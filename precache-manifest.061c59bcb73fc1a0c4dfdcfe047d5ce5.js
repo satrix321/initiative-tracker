f216af144d142cb5f030b428fc27f624be9204db
Krzysztof Szot <krzysztof.szot@cognifide.com>
2020-01-05 21:29:39 +0100

    Updates

---
+++ precache-manifest.061c59bcb73fc1a0c4dfdcfe047d5ce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82f65850f15282d9bf14fbce68ef6a46",
+    "url": "/initiative-tracker/index.html"
+  },
+  {
+    "revision": "437c6578cf1556e95da5",
+    "url": "/initiative-tracker/static/css/main.6b231528.chunk.css"
+  },
+  {
+    "revision": "676252c2973d14a209b1",
+    "url": "/initiative-tracker/static/js/2.e685c457.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/initiative-tracker/static/js/2.e685c457.chunk.js.LICENSE"
+  },
+  {
+    "revision": "437c6578cf1556e95da5",
+    "url": "/initiative-tracker/static/js/main.37bb1f50.chunk.js"
+  },
+  {
+    "revision": "7f4f3933364118808245",
+    "url": "/initiative-tracker/static/js/runtime-main.427ae88c.js"
+  }
+]);
